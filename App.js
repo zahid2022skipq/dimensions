c07d1b3d4bb350b855520f5be39a00de564c9ff4
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 11:33:26 +0500

    understanding flex box

--- App.js
+++ App.js
@@ -12,7 +12,12 @@ export default function App() {
       }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }}
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 300,
+          alignSelf: "flex-start",
+        }}
       />
       <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
