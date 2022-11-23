b9c2562fb49342c36c21c533b7d6da034b9c938f
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-23 11:30:20 +0500

    understanding flex box

--- App.js
+++ App.js
@@ -8,13 +8,13 @@ export default function App() {
         flex: 1,
         flexDirection: "row",
         justifyContent: "center", //main axis based on Direction
-        alignItems: "center",
+        alignItems: "baselines",
       }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
